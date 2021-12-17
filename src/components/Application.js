7e5c23391aa3101e6bacba1a83d290590891575b
Stefan Darrell Davis <60591525+Davisstef@users.noreply.github.com>
2021-12-16 19:00:17 -0500

    Refactored and cleaned up Application code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,19 +1,15 @@
 import React from "react";
 import "./Application.scss";
-import DayList from "./DayList"
+import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay} from "helpers/selectors"
+import { getAppointmentsForDay, getInterview, getInterviewersForDay} from "helpers/selectors";
 import useApplicationData from "hooks/useApplicationData";
 
-
 export default function Application(props) {
-
-const { state, setDay, bookInterview, cancelInterview } = useApplicationData()
-
-const appointmentsForDay = getAppointmentsForDay(state, state.day);
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
+  const appointmentsForDay = getAppointmentsForDay(state, state.day);
   const appointmentList = appointmentsForDay.map( appointment => {
     const interview = getInterview(state, appointment.interview);
-
     const interviewersForDay = getInterviewersForDay(state, state.day);
     return (
       <Appointment
@@ -30,27 +26,27 @@ const appointmentsForDay = getAppointmentsForDay(state, state.day);
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu"><DayList
-  days={state.days}
-  value={state.day}
-  onChange={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu"><DayList
+          days={state.days}
+          value={state.day}
+          onChange={setDay}
+        />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-         {appointmentList}
-        <Appointment key="last" time="5pm" />
+        {appointmentList}
+          <Appointment key="last" time="5pm" />
       </section>
     </main>
   )
